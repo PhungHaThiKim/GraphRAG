b74c42e68000007dea628118b7c26fa7108a07a2
PhungHaThiKim <hathikimphung0612@gmail.com>
2025-06-08 04:13:45 +0700

    delete widget

---
+++ t.py
@@ -0,0 +1,12 @@
+import nbformat
+
+input_path = "notebook\KnowGPT_colab.ipynb"
+output_path = "notebook\KnowGPT_colab2.ipynb"
+
+nb = nbformat.read(input_path, as_version=4)
+
+if "widgets" in nb.metadata and "state" not in nb.metadata["widgets"]:
+    nb.metadata["widgets"]["state"] = {}
+
+nbformat.write(nb, output_path)
+print("✅ Saved cleaned notebook to:", output_path)
